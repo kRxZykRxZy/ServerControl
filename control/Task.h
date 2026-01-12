e6fce8702676dcfb7e401d9a5cdac6c5eaaddaee
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2026-01-12 19:48:44 +0000

    Add TaskState enum to Task structure

--- control/Task.h
+++ control/Task.h
@@ -1,9 +1,14 @@
 #pragma once
 #include <string>
 
+enum class TaskState {
+    RUNNING,
+    FINISHED
+};
+
 struct Task {
     std::string id;
     std::string server;
     std::string command;
+    TaskState state;
 };
-
